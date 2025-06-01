adb2e63a462a4612829109f43a222960d246c937
Martin Fulop <hi@martinfulop.dev>
2025-06-01 10:55:01 +0200

    task 7 completed

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,13 +2,19 @@
 import * as cdk from "aws-cdk-lib";
 import { ProductServiceStack } from "../lib/product-service-stack";
 import { ImportServiceStack } from "../lib/import-service-stack";
+import { AuthorizationServiceStack } from "../lib/authorization-service-stack";
+import "dotenv/config";
 
 const app = new cdk.App();
+
+new AuthorizationServiceStack(app, "AuthorizationServiceStack", {});
+
 const productServiceStack = new ProductServiceStack(
   app,
   "ProductServiceStack",
   {}
 );
+
 new ImportServiceStack(app, "ImportServiceStack", {
   catalogItemsQueue: productServiceStack.catalogItemsQueue,
 });
